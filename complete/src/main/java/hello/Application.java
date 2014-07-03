5768c451400f73fb35900692d7f191fd101e0ed3
Craig Walls <cwalls@gopivotal.com>
2014-07-03 10:59:29 -0500

    Update for Spring Social Facebook 1.1.x and Spring Boot auto-configuration.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,19 +1,12 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.social.UserIdSource;
-import org.springframework.social.connect.ConnectionFactoryLocator;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.connect.web.ConnectController;
 
 @Configuration
 @EnableAutoConfiguration
-@Import(FacebookConfig.class)
 @ComponentScan
 public class Application {
 
--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -3,8 +3,8 @@ package hello;
 import javax.inject.Inject;
 
 import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.FacebookProfile;
 import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,8 +28,8 @@ public class HelloController {
         }
 
         model.addAttribute(facebook.userOperations().getUserProfile());
-        PagedList<FacebookProfile> friends = facebook.friendOperations().getFriendProfiles();
-        model.addAttribute("friends", friends);
+        PagedList<Post> homeFeed = facebook.feedOperations().getHomeFeed();
+        model.addAttribute("feed", homeFeed);
 
         return "hello";
     }
