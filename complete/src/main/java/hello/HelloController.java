b23c95c6d7b7b52c88f68746ceb31319077fe849
Dave Syer <dsyer@pivotal.io>
2016-08-25 17:22:19 +0100

    Update to Spring Boot 1.4 idioms

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,30 +1,27 @@
 package hello;
 
-import javax.inject.Inject;
-
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
     private Facebook facebook;
-	private ConnectionRepository connectionRepository;
+    private ConnectionRepository connectionRepository;
 
-    @Inject
     public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
         this.facebook = facebook;
-		this.connectionRepository = connectionRepository;
+        this.connectionRepository = connectionRepository;
     }
 
-    @RequestMapping(method=RequestMethod.GET)
+    @GetMapping
     public String helloFacebook(Model model) {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
             return "redirect:/connect/facebook";
